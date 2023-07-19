9009f60e2f41e13d2cd1cd70ca4197be69e3ed38
rolhuama <127351281+rolhuama@users.noreply.github.com>
2023-07-19 11:23:04 -0500

    .

--- build.sh
+++ build.sh
@@ -3,7 +3,7 @@
 mkdir aplicacion
 cd aplicacion
 git init
-git pull https://github.com/rolhuama/pruebasdecodigo.git
+git pull https://github.com/rolhuama/App.git
 gradle bootjar
 docker build -t aplicacion .
 docker tag aplicacion rolervengador/aplicacion
